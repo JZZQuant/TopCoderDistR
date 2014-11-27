6bc52a5859818b6768187c34566dae9c15179619
unknown <Sasikanth.Raghava@INQAWKS09.1e.local>
2014-11-27 17:32:27 +0530

    PLotting

--- vertica.R
+++ vertica.R
@@ -34,3 +34,13 @@ df<-data.frame(matrix(unlist(chunk,recursive=F),ncol=7,byrow=T), stringsAsFactor
 colnames(df)<-c("title","date","expire","amount","field","invest","abstract")
 df<-transform(df,date=as.Date(date," %B %d, %Y"),expire=as.Date(date," %B %d, %Y"),amount=as.numeric(amount),field=as.factor(field),invest=as.factor(invest))
 
+
+
+library(ggplot2)
+qplot(date,data=df[df$date>"1998-01-01",],geom='bar')
+qplot(date,amount,data=df[df$date>"1998-01-01",],geom='line')
+qplot(field,data=df[df$date>"1998-01-01" & df$amount >2000000,],geom='bar',weight=amount)
+head(sort(table(df$invest)))
+head(sort(table(df$field)))
+qplot(df$date[grepl('results',df$abstract)])
+qplot(date,data=df[df$date>"1998-01-01" & df$amount >2000000,],fill=as.factor(amount))
