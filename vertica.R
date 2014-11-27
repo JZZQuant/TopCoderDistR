0bdef252522df018ecf1018c77b117b72f002e24
unknown <Sasikanth.Raghava@INQAWKS09.1e.local>
2014-11-27 16:51:45 +0530

    Modified data transformation

--- vertica.R
+++ vertica.R
@@ -1,5 +1,11 @@
 #setwd("Data")
 articles<-list.files(pattern="\\.txt",recursive=T)
+title<-NULL
+date<-NULL
+expire<-NULL
+amount<-NULL
+field<-NULL
+invest<-NULL
 Structured<- function(article)
 {         
   lines<-readLines(article)
@@ -18,7 +24,7 @@ Structured<- function(article)
   title<-c(title,gsub(".*Title       :",'',lines[grepl('Title',lines)]))
   date<-c(date,gsub(".*Start Date  :",'',lines[grepl('Start Date',lines)]))
   expire<-c(expire,gsub(".*Expires     :",'',lines[grepl('Expires',lines)]))
-  amount<-c(amount,gsub(".*Total Amt.  :",'',lines[grepl('Amt',lines)]))
+  amount<-gsub("[^0-9]", "",c(amount,gsub(".*Total Amt.  :",'',lines[grepl('Amt',lines)])))
   field<-c(field,gsub(".*Fld Applictn:",'',lines[grepl('Fld',lines)]))
   invest<-c(invest,gsub(".*Investigator:",'',lines[grepl('Investigator',lines)]))
   c(title[[1]],date[[1]],expire[[1]],amount[[1]],field[[1]],invest[[1]],abstract[[1]])
@@ -26,3 +32,5 @@ Structured<- function(article)
 chunk<-lapply(articles[1:5000],function(x) Structured(x))
 df<-data.frame(matrix(unlist(chunk,recursive=F),ncol=7,byrow=T), stringsAsFactors =F)
 colnames(df)<-c("title","date","expire","amount","field","invest","abstract")
+df<-transform(df,date=as.Date(date," %B %d, %Y"),expire=as.Date(date," %B %d, %Y"),amount=as.numeric(amount),field=as.factor(field),invest=as.factor(invest))
+
